850e989963c19b6cf0880800f0eb94eb1bc1820b
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-14 01:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 const divide = (array, n) => {
   // Write your code here
 
-  n = parseInt(n);
+  let arr = array.split(" ").map(Number);
 
   let segment = [],
     i = 0;
 
   while (i < arr.length) {
-    let temp = subArray(arr, i, n);
-    segment.push(temp[0]);
-    i = temp[1];
+
+    let subSegment = [],
+      sum = 0;
+
+    while (i < arr.length && sum < n) {
+
+      if (arr[i] + sum <= n) {
+        subSegment.push(arr[i]);
+        sum += arr[i];
+      } else if (arr[i] > n) {
+        subSegment.push(arr[i]);
+        break;
+      } else if (arr[i] + sum > n) {
+        break;
+      }
+
+      i++;
+
+      if (sum === n) {
+        break;
+      }
+    }
+
+    segment.push(subSegment);
+    i++;
   }
 
   return segment;
 };
-
-function subArray(arr, index, maxSum) {
-  let subArray = [],
-    sum = 0;
-
-  while (index < arr.length) {
-    if (arr[index] + sum > maxSum) {
-      break;
-    }
-    subArray.push(arr[index]);
-    sum += arr[index];
-    index++;
-  }
-
-  return [subArray, index];
-}
 
 const arr = prompt("Enter an array of numbers which are space separated: ");
 const n = parseInt(prompt("Enter n: "));
